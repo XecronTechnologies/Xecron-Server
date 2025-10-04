276bb15fdd3cc6cffe330dcb71e462290e9f5cf2
csathishkumar <csathishkumar200@gmail.com>
2025-10-04 23:01:55 +0530

    updated firebase and supabase struture

--- server/services/firebaseService.js
+++ server/services/firebaseService.js
@@ -2,7 +2,7 @@ import admin from "firebase-admin";
 import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
-const serviceAccount = require("./serviceAccountKey.json");
+// const serviceAccount = require("./serviceAccountKey.json");
 
 // Initialize Firebase only once
 // if (!admin.apps.length) {
