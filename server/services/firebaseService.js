9c312bcf3ec14dcebabb62e8acad4b621e987efb
csathishkumar <csathishkumar200@gmail.com>
2025-10-04 22:56:47 +0530

    updated firebase and supabase struture

--- server/services/firebaseService.js
+++ server/services/firebaseService.js
@@ -2,7 +2,7 @@ import admin from "firebase-admin";
 import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
-// const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("./serviceAccountKey.json");
 
 // Initialize Firebase only once
 // if (!admin.apps.length) {
@@ -20,7 +20,7 @@ export class FirebaseService {
   "type": "service_account",
   "project_id": "xecron-client-management",
   "private_key_id": process.env.PRIVATE_KEY_ID,
-  "private_key": process.env.PRIVATE_KEY,
+  "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
   "client_email": process.env.CLIENT_EMAIL,
   "client_id": process.env.CLIENT_ID,
   "auth_uri": process.env.AUTH_URI,
