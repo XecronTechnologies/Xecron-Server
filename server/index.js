ae184d9658f8045d5c81dd1b1692b43eb4ca48b3
XecronTechnologies <xecrontech@gmail.com>
2025-10-25 22:59:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js";
 
 import googleDriveRoutes from "./routes/googleDriveRoutes.js";
 
